da4da83547a6e28e41ed25f5c78439a3237d3476
love-quinn <lucaslucal75@gmail.com>
2026-02-22 22:50:32 -0300

    fix: vitest.config ts error

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,28 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import path from "path";
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineVitestConfig } from "vitest/config";
+import { defineConfig as defineViteConfig, mergeConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: ["./setupTests.ts"],
-    include: ["**/__tests__/**/*.test.{ts,tsx}", "**/*.test.{ts,tsx}"],
-    globals: true,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
     },
   },
 });
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+    include: ["**/__tests__/**/*.test.{ts,tsx}", "**/*.test.{ts,tsx}"],
+    globals: true,
+  },
+});
+
+// ✅ single, latest export
+export default mergeConfig(viteConfig, vitestConfig);
