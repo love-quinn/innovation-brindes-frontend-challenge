e7494853ee8525beff1b62252c8b76f1d12d6bbb
love-quinn <lucaslucal75@gmail.com>
2026-02-22 22:03:27 -0300

    feat: redirect to login page on middleware

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,10 @@ import { AUTH_TOKEN_KEY } from "@/lib/auth-constants";
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
   if (pathname.startsWith("/produtos")) {
     const token = request.cookies.get(AUTH_TOKEN_KEY)?.value;
     if (!token) {
